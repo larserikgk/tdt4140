2a6b4bcc87a66ba58a89236fda11ee80b8d7a41e
unknown <v-91@hotmail.com>
2013-03-11 22:50:37 +0100

    EventPanel GUI

---
+++ fellesprosjekt/src/client/gui/EventPanel.java
@@ -0,0 +1,168 @@
+package client.gui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+public class EventPanel extends JPanel {
+	private JTextField textField;
+	private JTextField textField_1;
+	
+	//Disse variablene er de samme som i GUI_Main (fjerner dem senere)
+	static final Color main_bg_light_grey = new Color(Integer.parseInt("e2dfe0",16));
+	static final Color main_bg_dark_grey = new Color(Integer.parseInt("595959",16));
+	static final Color dark_grey = new Color(Integer.parseInt("181818",16));
+	static final Color light_blue = new Color(Integer.parseInt("159ec0",16));
+	static final Color orange = new Color(Integer.parseInt("c53d2c",16));
+	static final Color notification_red = new Color(Integer.parseInt("ff0000",16));
+	static final Font FONT_TITLE1 = new Font("Myriad Pro", Font.BOLD, 18);
+	static final Font FONT_TEXT1 = new Font("Myriad Pro", Font.PLAIN, 18);
+	static final Font FONT_TEXT2 = new Font("Myriad Pro", Font.PLAIN, 16);
+	
+	public EventPanel() {
+		setBackground(dark_grey);
+		Border border = BorderFactory.createLineBorder(orange);
+		setBorder(border);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 241, 117, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{17, 0, 0, 0, 0, 73, 73, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblCreateEvent = new JLabel("Create event");
+		lblCreateEvent.setFont(FONT_TEXT1);
+		lblCreateEvent.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblCreateEvent = new GridBagConstraints();
+		gbc_lblCreateEvent.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCreateEvent.gridx = 1;
+		gbc_lblCreateEvent.gridy = 1;
+		add(lblCreateEvent, gbc_lblCreateEvent);
+		
+		JLabel lblTitle = new JLabel("Title");
+		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+		lblTitle.setForeground(Color.WHITE);
+		gbc_lblTitle.anchor = GridBagConstraints.WEST;
+		gbc_lblTitle.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTitle.gridx = 1;
+		gbc_lblTitle.gridy = 2;
+		add(lblTitle, gbc_lblTitle);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 2;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblStart = new JLabel("Start");
+		lblStart.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblStart = new GridBagConstraints();
+		gbc_lblStart.anchor = GridBagConstraints.WEST;
+		gbc_lblStart.insets = new Insets(0, 0, 5, 5);
+		gbc_lblStart.gridx = 1;
+		gbc_lblStart.gridy = 3;
+		add(lblStart, gbc_lblStart);
+		
+		JLabel lblEnd = new JLabel("End");
+		lblEnd.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblEnd = new GridBagConstraints();
+		gbc_lblEnd.anchor = GridBagConstraints.WEST;
+		gbc_lblEnd.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEnd.gridx = 1;
+		gbc_lblEnd.gridy = 4;
+		add(lblEnd, gbc_lblEnd);
+		
+		JLabel lblParticipants = new JLabel("Participants");
+		lblParticipants.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblParticipants = new GridBagConstraints();
+		gbc_lblParticipants.anchor = GridBagConstraints.WEST;
+		gbc_lblParticipants.gridheight = 2;
+		gbc_lblParticipants.insets = new Insets(0, 0, 5, 5);
+		gbc_lblParticipants.gridx = 1;
+		gbc_lblParticipants.gridy = 5;
+		add(lblParticipants, gbc_lblParticipants);
+		
+		JList list = new JList();
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.gridheight = 2;
+		gbc_list.insets = new Insets(0, 0, 5, 5);
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 2;
+		gbc_list.gridy = 5;
+		add(list, gbc_list);
+		
+		JButton btnAddParticipants = new JButton("Add participants");
+		GridBagConstraints gbc_btnAddParticipants = new GridBagConstraints();
+		gbc_btnAddParticipants.gridwidth = 2;
+		gbc_btnAddParticipants.anchor = GridBagConstraints.SOUTHWEST;
+		gbc_btnAddParticipants.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAddParticipants.gridx = 3;
+		gbc_btnAddParticipants.gridy = 5;
+		add(btnAddParticipants, gbc_btnAddParticipants);
+		
+		JButton btnDeleteParticipants = new JButton("Delete participants");
+		GridBagConstraints gbc_btnDeleteParticipants = new GridBagConstraints();
+		gbc_btnDeleteParticipants.gridwidth = 2;
+		gbc_btnDeleteParticipants.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnDeleteParticipants.insets = new Insets(0, 0, 5, 5);
+		gbc_btnDeleteParticipants.gridx = 3;
+		gbc_btnDeleteParticipants.gridy = 6;
+		add(btnDeleteParticipants, gbc_btnDeleteParticipants);
+		
+		JLabel lblLocation = new JLabel("Location");
+		lblLocation.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblLocation = new GridBagConstraints();
+		gbc_lblLocation.anchor = GridBagConstraints.WEST;
+		gbc_lblLocation.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLocation.gridx = 1;
+		gbc_lblLocation.gridy = 7;
+		add(lblLocation, gbc_lblLocation);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 7;
+		add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnFindRoom = new JButton("Find room");
+		GridBagConstraints gbc_btnFindRoom = new GridBagConstraints();
+		gbc_btnFindRoom.gridwidth = 2;
+		gbc_btnFindRoom.anchor = GridBagConstraints.WEST;
+		gbc_btnFindRoom.insets = new Insets(0, 0, 5, 5);
+		gbc_btnFindRoom.gridx = 3;
+		gbc_btnFindRoom.gridy = 7;
+		add(btnFindRoom, gbc_btnFindRoom);
+		
+		JButton btnCancel = new JButton("Cancel");
+		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
+		gbc_btnCancel.anchor = GridBagConstraints.EAST;
+		gbc_btnCancel.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCancel.gridx = 3;
+		gbc_btnCancel.gridy = 9;
+		add(btnCancel, gbc_btnCancel);
+		
+		JButton btnCreate = new JButton("Create");
+		GridBagConstraints gbc_btnCreate = new GridBagConstraints();
+		gbc_btnCreate.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCreate.gridx = 4;
+		gbc_btnCreate.gridy = 9;
+		add(btnCreate, gbc_btnCreate);
+	}
+
+}
--- fellesprosjekt/src/client/gui/GUI_Main.java
+++ fellesprosjekt/src/client/gui/GUI_Main.java
@@ -1,59 +1,63 @@
 package client.gui;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.GridLayout;
-import javax.swing.JProgressBar;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import javax.swing.JLabel;
-import javax.swing.BoxLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.CardLayout;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
-import javax.swing.JScrollBar;
-import net.miginfocom.swing.MigLayout;
-import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
-import javax.swing.JTextPane;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+
+import com.jgoodies.forms.factories.DefaultComponentFactory;
 
 public class GUI_Main extends JPanel {
 	
-	Color main_bg_light_grey = new Color(Integer.parseInt("e2dfe0",16));
-	Color main_bg_dark_grey = new Color(Integer.parseInt("595959",16));
-	Color dark_grey = new Color(Integer.parseInt("181818",16));
-	Color light_blue = new Color(Integer.parseInt("159ec0",16));
-	Color orange = new Color(Integer.parseInt("c53d2c",16));
-	Color notification_red = new Color(Integer.parseInt("ff0000",16));
+	//TODO: Flett disse fargene sammen med de som er i Settings.java
+	static final Color main_bg_light_grey = new Color(Integer.parseInt("e2dfe0",16));
+	static final Color main_bg_dark_grey = new Color(Integer.parseInt("595959",16));
+	static final Color dark_grey = new Color(Integer.parseInt("181818",16));
+	static final Color light_blue = new Color(Integer.parseInt("159ec0",16));
+//	static final Color dark_grey = Settings.CELL_UNSELECTED_COLOR;
+//	static final Color light_blue = Settings.CELL_SELECTED_COLOR;
+	static final Color orange = new Color(Integer.parseInt("c53d2c",16));
+	static final Color notification_red = new Color(Integer.parseInt("ff0000",16));
+	
+	//TODO: Flytt også dette over til Settings.java
+	static final Font FONT_TITLE1 = new Font("Myriad Pro", Font.BOLD, 18);
+	static final Font FONT_TEXT1 = new Font("Myriad Pro", Font.PLAIN, 18);
+	static final Font FONT_TEXT2 = new Font("Myriad Pro", Font.PLAIN, 16);
 	
     public static void main (String args[]) { 
         JFrame frame = new JFrame("FP Calendar"); 
-        frame.getContentPane().add(new GUI_Main()); 
+        frame.getContentPane().add(new GUI_Main());
         frame.pack(); 
-        frame.setVisible(true);   
+        int frameWidth = 1000;
+        int frameHeight = 500;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setBounds((int) screenSize.getWidth() - frameWidth, 0, frameWidth, frameHeight);
+        frame.setVisible(true);
         }
     
-    
 	public GUI_Main() {
-		setBackground(Color.WHITE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{519, 205, 10, 0};
-		gridBagLayout.rowHeights = new int[]{41, 27, 0, 26, 293, 233, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{26, 670, 257, 10, 0};
+		gridBagLayout.rowHeights = new int[]{41, 27, 0, 26, 293, 233, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
-		
 		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
 		panel.setBackground(dark_grey);
-		gbc_panel.gridwidth = 3;
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridwidth = 4;
 		gbc_panel.insets = new Insets(0, 0, 5, 0);
 		gbc_panel.fill = GridBagConstraints.BOTH;
 		gbc_panel.gridx = 0;
@@ -68,18 +72,20 @@ public class GUI_Main extends JPanel {
 		panel.add(lblFileEdit);
 		
 		JLabel lblMarch = DefaultComponentFactory.getInstance().createTitle("March 2013");
+		lblMarch.setFont(FONT_TITLE1);
 		GridBagConstraints gbc_lblMarch = new GridBagConstraints();
 		gbc_lblMarch.anchor = GridBagConstraints.WEST;
 		gbc_lblMarch.insets = new Insets(0, 0, 5, 5);
-		gbc_lblMarch.gridx = 0;
+		gbc_lblMarch.gridx = 1;
 		gbc_lblMarch.gridy = 1;
 		add(lblMarch, gbc_lblMarch);
 		
 		JLabel lblJensStoltenberg = DefaultComponentFactory.getInstance().createLabel("Jens Stoltenberg");
+		lblJensStoltenberg.setFont(FONT_TEXT2);
 		GridBagConstraints gbc_lblJensStoltenberg = new GridBagConstraints();
 		gbc_lblJensStoltenberg.anchor = GridBagConstraints.WEST;
 		gbc_lblJensStoltenberg.insets = new Insets(0, 0, 5, 5);
-		gbc_lblJensStoltenberg.gridx = 1;
+		gbc_lblJensStoltenberg.gridx = 2;
 		gbc_lblJensStoltenberg.gridy = 1;
 		add(lblJensStoltenberg, gbc_lblJensStoltenberg);
 		
@@ -88,39 +94,62 @@ public class GUI_Main extends JPanel {
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
 		gbc_panel_2.fill = GridBagConstraints.BOTH;
 		gbc_panel_2.gridheight = 3;
-		gbc_panel_2.insets = new Insets(0, 0, 0, 5);
-		gbc_panel_2.gridx = 1;
+		gbc_panel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_2.gridx = 2;
 		gbc_panel_2.gridy = 3;
 		add(panel_2, gbc_panel_2);
-		FlowLayout fl_panel_2 = new FlowLayout(FlowLayout.CENTER, 5, 5);
-		panel_2.setLayout(fl_panel_2);
+		GridBagLayout gbl_panel_2 = new GridBagLayout();
+		gbl_panel_2.columnWidths = new int[]{287, 0};
+		gbl_panel_2.rowHeights = new int[]{551, 0};
+		gbl_panel_2.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_2.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_2.setLayout(gbl_panel_2);
 		
 		JLabel lblUpcomingEvents = DefaultComponentFactory.getInstance().createLabel("Upcoming events");
+		lblUpcomingEvents.setFont(FONT_TEXT1);
+		lblUpcomingEvents.setVerticalAlignment(SwingConstants.TOP);
 		lblUpcomingEvents.setForeground(Color.WHITE);
-		panel_2.add(lblUpcomingEvents);
+		GridBagConstraints gbc_lblUpcomingEvents = new GridBagConstraints();
+		gbc_lblUpcomingEvents.fill = GridBagConstraints.BOTH;
+		gbc_lblUpcomingEvents.gridx = 0;
+		gbc_lblUpcomingEvents.gridy = 0;
+		panel_2.add(lblUpcomingEvents, gbc_lblUpcomingEvents);
 		
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.insets = new Insets(0, 0, 0, 5);
-		gbc_panel_1.gridx = 0;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_1.gridx = 1;
 		gbc_panel_1.gridy = 5;
 		add(panel_1, gbc_panel_1);
 		panel_1.setBackground(dark_grey);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{203, 107, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_1.rowHeights = new int[]{23, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWidths = new int[]{203, 107, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{23, 0, 0, 0, 0, 0, 0, 0, 10, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
 		panel_1.setAlignmentX(RIGHT_ALIGNMENT);
-		
+										
 		JButton btnCreateEvent = new JButton("+ Create event");
+		btnCreateEvent.setFont(FONT_TEXT1);
 		GridBagConstraints gbc_btnCreateEvent = new GridBagConstraints();
-		gbc_btnCreateEvent.insets = new Insets(0, 0, 0, 5);
+		gbc_btnCreateEvent.insets = new Insets(0, 0, 5, 5);
 		gbc_btnCreateEvent.anchor = GridBagConstraints.NORTHWEST;
-		gbc_btnCreateEvent.gridx = 6;
-		gbc_btnCreateEvent.gridy = 6;
+		gbc_btnCreateEvent.gridx = 9;
+		gbc_btnCreateEvent.gridy = 7;
 		panel_1.add(btnCreateEvent, gbc_btnCreateEvent);
+		
+		btnCreateEvent.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("CLICKED");
+				JFrame frame = new JFrame("Create event");
+				frame.getContentPane().add(new EventPanel());
+				frame.pack();
+				frame.setVisible(true);
+				
+			}
+		});
 	}
 }
