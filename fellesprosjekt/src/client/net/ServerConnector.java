b6d96007360db6ea0d31c50fd01d070594bbad71
Lars Erik G-K <larserikgk@gmail.com>
2013-03-12 20:53:04 +0100

    Added skeleton of ClientHandler

--- fellesprosjekt/src/client/net/ServerConnector.java
+++ fellesprosjekt/src/client/net/ServerConnector.java
@@ -1,9 +1,80 @@
 package client.net;
 
-public class ServerConnector {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Properties;
 
-	public ServerConnector() {
-		// TODO Auto-generated constructor stub
+public class ServerConnector {
+	private Socket socket;
+	private Object caller;
+	private String url;
+	private String username;
+	private int port;
+	private InputStream input;
+	private OutputStream output;
+	
+	public ServerConnector(Properties settings, String username) {
+		this.url = settings.getProperty(url);
+		this.username = username;
+		this.port = port;
+	}
+	
+	public boolean start() {
+		try {
+			socket = new Socket(url, port);
+		} 
+		catch(Exception ec) {
+			return false;
+		}
+
+
+
+		try
+		{
+			input  = socket.getInputStream();
+			output = socket.getOutputStream();
+		}
+		catch (IOException eIO) {
+			return false;
+		}
+
+		new ListenFromServer().start();
+		try
+		{
+//			output.writeObject(username);
+		}
+		catch (IOException eIO) {
+			return false;
+		}
+		return true;
+	}
+
+
+	
+
+	
+	private class ListenFromServer extends Thread {
+		public void run() {
+			while(true) {
+				try {
+					Response res = (Response) sInput.readObject();			
+					caller.handleResponse(res);
+				}
+				catch(IOException e) {
+					display("Server has close the connection: " + e);
+					if(caller != null) 
+						caller.connectionFailed();
+					break;
+				}
+				catch(ClassNotFoundException e2) {/** Ignore errors */
+				}
+			}
+		}
 	}
 
 }
--- fellesprosjekt/src/server/net/ClientHandler.java
+++ fellesprosjekt/src/server/net/ClientHandler.java
@@ -1,9 +1,43 @@
 package server.net;
 
-public class ClientHandler {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import server.db.SqlConnector;
+import server.logic.Server;
 
-	public ClientHandler() {
+
+public class ClientHandler implements Runnable{
+	private Socket socket;
+	private OutputStream output;
+	private InputStream input;
+	private Server server;
+	private String username;
+	private boolean keepGoing = true;
+	private SqlConnector database;
+	
+	
+	public ClientHandler(Socket socket, Server server) {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void run() {
+		try{
+			output = socket.getOutputStream();
+			input  = socket.getInputStream();
+//			username = (String) input.readObject();
+
+			while(keepGoing){
+
+				
+			}
+		}
+		catch(IOException e) {
+			//TODO exceptions
+		} 
+
+	}
 }
