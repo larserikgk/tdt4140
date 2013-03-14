2b13063894c58537f7a1d970a028135e3e7f271f
Haakon <haakon@haakon.(none)>
2013-03-14 22:41:27 +0100

    funker ikke, mye blir gjort på Fredag

--- fellesprosjekt/src/common/tests/testSqlConnectior.java
+++ fellesprosjekt/src/common/tests/testSqlConnectior.java
@@ -1,10 +1,12 @@
 package common.tests;
 
+import java.util.Date;
 import java.util.Properties;
 import java.sql.*; 
 
 import server.db.SQLquerries;
 import server.db.SqlConnector;
+
 
 public class testSqlConnectior {
 
@@ -13,8 +15,9 @@ public class testSqlConnectior {
 
 	/**
 	 * @param args
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 			set = new Properties();
 			set.setProperty("url", "127.0.0.1"); 
 			set.setProperty("username", "root"); 
@@ -22,21 +25,49 @@ public class testSqlConnectior {
 			set.setProperty("password", "haakon"); 
 			
 			sql = new SQLquerries(set);
-			addUsers(10);
+		
+			
+			Date dato1 = new Date(2010,06,25,12,50);
+			Date dato2 = new Date(2010,06,25,12,50);
+			String des = "DETTE ER ET MOTHERFUCKINGS MØTE!";
+			
+			
+		// 	addSomeUsers(10);	
+		//	addSomeEvent(2013,05,25,20,60,2013,06,25,21,20,"haakon1");
+		//	addSomeEventDes("DETTE ER ET MOTHERFUCKINGS MØTE!");
+		//	addSomeEventDes(dato1, dato2, "haakon1", des, "her");
+			
+			String get = "SELECT * FROM User";
+			sql.getFromUser(get, 3);
 			 
 			
 	}
-	public static void addUsers(int t)
+	
+	
+	public static void addSomeUsers(int t) throws SQLException
 	{
 		
 		String test;
+		
 		for(int i = 0; i < t; i++)
 		{
-			test = "INSERT INTO User(username, passord, name) VALUES ('derpe" + i +"','DerpeMer', 'Derpus')";
-			sql.addSome(test);
+//			test = "INSERT INTO User(username, passord, name) VALUES ('derpe" + i +"','DerpeMer', 'Derpus')";
+			sql.addSomeUser("haakon" + i, "hei", "haakon");
 		}
 		
 	}
+	private static void addSomeEvent(int year, int month, int day, int hour, int min, 
+			int dyear,int dmonth, int dday, int dhour, int dmin, String userName)
+	{
+		sql.addSomeEvent(year, month, day, hour, min, dyear, dmonth, day, dhour, dmin, userName); 
+	}
+	private static void addSomeEventDes(Date dateStart, Date dateEnd, 
+			String userName, String description, String location)
+	{
+		sql.addSomeEvent(dateStart, dateEnd, userName, description, location); 
+		 
+	}
+	
 	
 
 }
--- fellesprosjekt/src/server/db/SqlConnector.java
+++ fellesprosjekt/src/server/db/SqlConnector.java
@@ -7,16 +7,14 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Properties;
-import common.models.Event;
 
 public class SqlConnector {
 	private String url;
 	private String username;
 	private String database;
 	private String password;
-	protected Connection conn;
+	protected static Connection conn;
 
 	public SqlConnector(Properties settings) {
 		this.url = settings.getProperty("url");
@@ -35,15 +33,6 @@ public class SqlConnector {
 		{
 			System.err.println ("Cannot connect to database server"+e.toString());
 		}
-	}
-	
-	public ArrayList<Event> getEvents(int userId, int amount) {
-		ArrayList<Event> list = new ArrayList<Event>();
-		String[][] r = processQuery("SELECT * FROM access_rights WHERE user_id = " + userId + "");
-		for (int i = 0; i < r.length; i++) {
-			list.add(new Event());
-		}
-		return list;
 	}
 	
 	/** A method for processing SELECT queries easier. Returns a String[][] instead of using result sets given 
