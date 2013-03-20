f0ba8fbe551a941a03eba9d1ad67d27a1313d263
kwrl <kwrl@kwrl-VPCYB2M1E.(none)>
2013-03-20 12:32:16 +0100

    Startet testing av Klient-Server-forbindelse.

--- fellesprosjekt/src/server/logic/Server.java
+++ fellesprosjekt/src/server/logic/Server.java
@@ -18,10 +18,10 @@ public class Server {
 		settings = new Properties();
 		settings.setProperty("handlerPort", Integer.toString(handlerPort));
 		settings.setProperty("pushPort", Integer.toString(pushPort));
-		settings.setProperty("url","");
-		settings.setProperty("database", "");
-		settings.setProperty("username", "");
-		settings.setProperty("password", "");
+		settings.setProperty("url","129.241.127.13");
+		settings.setProperty("database", "prosjekt");
+		settings.setProperty("username", "root");
+		settings.setProperty("password", "stiligPassord");
 		connectedClients = new ArrayList<NotificationHandler>();
 
 	}
