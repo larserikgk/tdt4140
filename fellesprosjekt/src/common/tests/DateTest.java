6b2b20ab690a1db61ea01b3bec0a57b8a82bf0ba
kwrl <kwrl@kwrl-VPCYB2M1E.(none)>
2013-03-21 17:20:31 +0100

    Date bug fixed

---
+++ fellesprosjekt/src/common/tests/DateTest.java
@@ -0,0 +1,15 @@
+package common.tests;
+
+import java.util.Date;
+
+public class DateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Date(new Date().getTime()));
+	}
+
+}
