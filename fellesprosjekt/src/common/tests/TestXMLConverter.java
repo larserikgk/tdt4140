85dbf70dc5ae656466149225d5119ae95a195df3
unknown <larserikgk@gmail.com>
2013-03-21 15:42:45 +0100

    Added xmltest of eventlist

--- fellesprosjekt/src/common/tests/TestXMLConverter.java
+++ fellesprosjekt/src/common/tests/TestXMLConverter.java
@@ -21,18 +21,17 @@ public class TestXMLConverter
 	static Event party	= new Event(derp,1,date1,date2,"Fest","My place","Epic", new ArrayList<User>(), room1);
 	static Notification not = new Notification(NotificationType.INVITATION, "Awesome parteh", party);
 	static ArrayList<User> participants = new ArrayList<User>();
+	static ArrayList<Event> events = new ArrayList<Event>();
 	static XMLConverter converter = new XMLConverter();
 	static Document doc;
 	
 	public static void main(String[] args)
 	{
-		participants.add(new User("lars1","123","lars1"));
-		participants.add(new User("lars4","123","lars2"));
-		participants.add(new User("lars3","123","lars3"));
-		participants.add(new User("lars2","123","lars4"));
+		
 		
 //		testEventConversion();
 //		testListConversion();
+		testEventListConversion();
 	}
 	
 	public static void testUserConversion()
@@ -92,7 +91,11 @@ public class TestXMLConverter
 		System.out.println(date2);
 	}
 	
-	public static void testListConversion() {
+	public static void testUserListConversion() {
+		participants.add(new User("lars1","123","lars1"));
+		participants.add(new User("lars4","312","lars2"));
+		participants.add(new User("lars3","321","lars3"));
+		participants.add(new User("lars2","213","lars4"));
 		doc = converter.getNewDocument();
 		converter.userListToDOMElement(participants, doc, null);
 		
@@ -100,7 +103,25 @@ public class TestXMLConverter
 		
 		ArrayList<User> users = converter.constructUserListFromNode(doc.getFirstChild());
 		for (int i = 0; i < users.size(); i++) {
-			System.out.println(users.get(i).getName());
+			System.out.println("name: "+ users.get(i).getName() +"  username: "+ users.get(i).getUsername() +" password: "+users.get(i).getPassword());
+		}
+	}
+	
+	public static void testEventListConversion() {
+		participants.add(new User("per","1","per"));
+		participants.add(new User("pål","2","pål"));
+		participants.add(new User("askeladden","3","askeladden"));
+		
+		events.add(new Event(new User("grasdalk", "Lars Erik",""),1,new Date(450000),new Date(460000), "møte", "p15", "p15", participants, new Room("421", 30)));
+		
+		doc = converter.getNewDocument();
+		converter.eventListToDOMElement(events, doc, null);
+		
+		System.out.println(converter.DOMDocumentToString(doc));
+		
+		ArrayList<Event> events = converter.constructEventListFromNode(doc.getFirstChild());
+		for (int i = 0; i < events.size(); i++) {
+			System.out.println("name: "+ events.get(i).getAdmin().getName() +"  start: "+ events.get(i).getStart().toString() +" end: "+events.get(i).getEnd().toString());
 		}
 	}
 }
